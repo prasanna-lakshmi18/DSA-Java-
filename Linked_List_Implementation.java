21e5e88d8d48bc4c708c5b53a302964c4d04bba2
Prasanna Lakshmi <167883557+prasanna-lakshmi18@users.noreply.github.com>
2025-06-11 11:34:12 +0530

    Add files via upload

---
+++ Linked_List_Implementation.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node refe;
+    
+    Node(int data){
+        this.data=data;
+        this.refe=null;
+    }
+}
+
+class LinkedList{
+    private Node head;
+    
+    LinkedList(){
+        this.head=null;
+        //Enabling first element to be null
+    }
+    public void printList(){
+        if(head==null){
+            System.out.println("Linked list is empty");
+        }
+        else{
+            Node current=head;
+            while(current!= null){
+                System.out.print(current.data+"---->");
+                current=current.refe;
+            }
+            System.out.println("null");
+        }
+    }
+    public void addAtBegin(int data){
+        Node newNode=new Node(data);
+        newNode.refe=head;
+        head=newNode;
+    }
+}
+
+
+class LinkedListDemo {
+    public static void main(String[] args) {
+        //System.out.println("Try programiz.pro");
+        LinkedList ll=new LinkedList();
+        ll.addAtBegin(10);
+        ll.addAtBegin(20);
+        ll.printList();
+    }
+}
---
+++ Rem duplicates from sorted array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        int temp=1;
+        for(int i=0;i<n;i++)    {
+            if(nums[temp]!=nums[i]){
+                nums[temp]=nums[i];
+                temp++;
+            }
+        }
+        return temp;
+    }
+}
